fc7fc5bc3a89e4f81de9163c6bceabad5aba89b6
Isidro Arias <isidroariass@hotmail.es>
2024-09-14 17:13:00 +0200

    wip: not yet fixed null adress

--- pmparser_mine.c
+++ pmparser_mine.c
@@ -12,18 +12,30 @@ int _pmparser_parse_line(char * line, procmaps_struct * entry){
   return 0;
 }
 
+#include <assert.h>
 // for easier testing
 procmaps_iterator* _pmparser_parse_stream(FILE * stream){
   
   char line[PROCMAPS_LINE_MAX_LENGTH];
 
+  procmaps_iterator * it = malloc(sizeof(procmaps_iterator));
+  procmaps_struct * entry =NULL,*prev = NULL;
+
   while (fgets(line, sizeof(line), stream) !=NULL){
 
+    // TODO: handle errors
     procmaps_struct * entry = malloc(sizeof(procmaps_struct));
+    assert(entry!=NULL);
+    _pmparser_parse_line(line, entry);
+    if (prev != NULL) // unless first iteration
+      prev->next = entry;
+    if (it->head == NULL) // first iteration
+      it->head = entry;
+    prev = entry;
 
   }
-  return NULL;
-
+  entry->next = NULL;
+  return it;
 }
 
 procmaps_iterator* pmparser_parse(int pid){
@@ -42,3 +54,29 @@ procmaps_iterator* pmparser_parse(int pid){
    
 }
 
+procmaps_struct* pmparser_next(procmaps_iterator* p_procmaps_it){
+  // consumo aun más bajo de memoria seria guardar el stream y hacer parse_line aquí. Esa es la ventaja de los iterators con respecto a los arrays. De lo contrario (por ejemplo si se quiere acceder a todos los elementos a la vez), sería mejor un array y usar realloc.
+  // TODO: mirar implementación de getifaddr o alguna otra API que hace uso de linked lists.
+  procmaps_struct* res;
+  if(p_procmaps_it->current == NULL)
+    res=p_procmaps_it->head;
+  else
+    res = p_procmaps_it->current;
+  p_procmaps_it->current = res->next;
+  return res;
+}
+
+
+void pmparser_free(procmaps_iterator* p_procmaps_it){
+
+  procmaps_struct* next;
+  procmaps_struct* current = p_procmaps_it->head;
+
+  while(current!=NULL) {
+    procmaps_struct* next = current->next;
+    free(current);
+    current = next;
+  };
+
+  free(p_procmaps_it);
+}
--- test.c
+++ test.c
@@ -2,8 +2,9 @@
 #include "pmparser.h"
 
 int _pmparser_parse_line(char * line, procmaps_struct * entry);
+procmaps_iterator* _pmparser_parse_stream(FILE * stream);
 
-int main(void){
+void test_parse_line(void){
   // from man pages example:
   char line[] = "00400000-00452000 r-xp 00000000 08:02 173521      /usr/bin/dbus-daemon";
   procmaps_struct entry;
@@ -16,4 +17,29 @@ int main(void){
   assert(strcmp(entry.dev, "08:02") == 0);
   assert(entry.inode == 173521);
   assert(strcmp(entry.pathname, "/usr/bin/dbus-daemon") == 0);
+}
+
+void test_parse_stream(void){
+
+  char contents[] = "00400000-00452000 r-xp 00000000 08:02 173521      /usr/bin/dbus-daemon\n"
+    "00651000-00652000 r--p 00051000 08:02 173521      /usr/bin/dbus-daemon\n";
+  FILE * stream = fmemopen(contents, sizeof(contents), "r");
+  procmaps_iterator *it= _pmparser_parse_stream(stream);
+  procmaps_struct *entry = NULL;
+  entry = pmparser_next(it);
+  assert(entry->addr_start == (void *)0x00400000);
+  entry = pmparser_next(it);
+  assert(entry->addr_start == (void *)0x00651000);
+  entry = pmparser_next(it);
+  assert(entry == NULL);
+  pmparser_free(it);
+}
+
+void test_process(void){
+  //TODO: read maps of this process and supply check that is not null.
+}
+
+int main(void){
+  test_parse_line();
+  test_parse_stream();
 }
